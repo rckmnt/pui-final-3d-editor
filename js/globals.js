baad05d4f8b7e85baf20656012f7188cb324d88c
scott <scottl318@gmail.com>
2017-11-23 00:13:24 -0500

    css progress, basic js variables

---
+++ js/globals.js
@@ -0,0 +1,15 @@
+// globals
+
+const blue = "#4A76B2"
+const faintBlue = "rgba(99, 143, 190, 0.2)"
+const brown = "#201A16"
+const white = "#FFFFFF"
+const green = "#32A621"
+const red = "#BF0015"
+
+const strokeWeight = 6
+const controlCircleRadius = 10
+const controlCircleOffset = 10
+
+const headerHeight = 60
+const standardPadding = 20
