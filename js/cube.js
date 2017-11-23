eecb13385666563a3d064c10a213a69e4bcb63e8
commissionergordon <scottl318@gmail.com>
2017-11-23 09:42:04 -0500

    basic cube inserted into DOM, success

--- js/cube.js
+++ js/cube.js
@@ -1,46 +1,65 @@
 
-	  /* Environment */
+    /* Environment */
 
-		const axisHelper = new THREE.AxesHelper(50);
+	const axisHelper = new THREE.AxesHelper(50);
 
-		const planeGeometry = new THREE.CircleGeometry(50, 40);
-		const planeMaterial = new THREE.MeshLambertMaterial({
-			color: 0x00ffff,
-			transparent: true,
-			opacity: 0.5,
-			side: THREE.DoubleSide
-		});
+	const planeGeometry = new THREE.CircleGeometry(50, 40);
+	const planeMaterial = new THREE.MeshLambertMaterial({
+		color: 0x00ffff,
+		transparent: true,
+		opacity: 0.5,
+		side: THREE.DoubleSide
+	});
 
-		const plane = new THREE.Mesh(planeGeometry, planeMaterial);
-		plane.receiveShadow = true;
-		plane.rotation.x = -Math.PI / 2;
-		plane.position.set(0, 0, 0);
+	const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+	plane.receiveShadow = true;
+	plane.rotation.x = -Math.PI / 2;
+	plane.position.set(0, 0, 0);
 
-		// Scene
-		var scene = new THREE.Scene();
-		var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+	// Scene
+	var scene = new THREE.Scene();
+	var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
 
-		// Renderer
-		var renderer = new THREE.WebGLRenderer({ antialias: true });
-		renderer.setClearColor(0xffffff, 1);
-		renderer.setSize( window.innerWidth, window.innerHeight);
+	/* Lights */
 
+	const lights = (function() {
+	  const light1 = new THREE.DirectionalLight(white, 0.2)
+	  light1.position.set(10, 10, 10)
 
-		// DOM stuff
-		var container = document.getElementById( 'canvas' );
-		container.appendChild( renderer.domElement );
-		document.body.appendChild( container );
+	  const light2 = new THREE.DirectionalLight(white, 0.2)
+	  light2.position.set(-10, -10, -10)
 
-		var geometry = new THREE.BoxGeometry( 1, 3, 1 );
-		var material = new THREE.MeshBasicMaterial( { color: 0x00ff00, wireframe: true } );
-		var cube = new THREE.Mesh( geometry, material );
-		scene.add( cube );
-		scene.add( plane );
-		scene.add( axisHelper );
-		cube.rotation.y += 0.2;
-		cube.rotation.x += 1.2;
-		camera.position.z = 5;
-		renderer.render(scene, camera);
+	  const light3 = new THREE.AmbientLight(blue)
+
+	  return [light1, light2, light3]
+	})()
+
+	lights.forEach(light => {
+		scene.add(light)
+	})
+
+	// Renderer
+	var renderer = new THREE.WebGLRenderer({ antialias: true });
+	renderer.setClearColor(0xffffff, 1);
+	renderer.setSize( window.innerWidth, window.innerHeight);
+
+	// Assemble scene
+	var geometry = new THREE.BoxGeometry( 1, 3, 1 );
+	var material = new THREE.MeshLambertMaterial( { color: 0x00ff00, wireframe: false } );
+	var cube = new THREE.Mesh( geometry, material );
+	scene.add( cube );
+	scene.add( plane );
+	scene.add( axisHelper );
+	cube.rotation.y += 0.2;
+	cube.rotation.x += 1.2;
+	camera.position.z = 5;
+	renderer.render(scene, camera);
+
+	// DOM stuff
+	var container = document.getElementById( 'canvas' );
+	container.appendChild( renderer.domElement );
+	document.body.appendChild( container );
+
 
 function render() {
     cube.rotation.y += 0.01;
