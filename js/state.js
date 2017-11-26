080489369da6f8499e964cbc26f1a6dff93bf347
commissionergordon <scottl318@gmail.com>
2017-11-25 21:56:25 -0500

    toggle selector working

--- js/state.js
+++ js/state.js
@@ -17,3 +17,25 @@ const initialState = {
     windowWidth: 0
   }
 }
+
+// Toggle Which Y Type to Add
+
+var currentY = "small";
+
+function toggleYPiece(box) {
+    if (box.id == "large_drag"){
+	    $("#large_drag").css("border-color", "black");
+	    $("#large_drag").css("border-style", "solid");
+	    $("#small_drag").css("border-style", "none");
+		log("Large");
+		currentY = "large";
+		return "large";
+	} else if (box.id == "small_drag"){
+	    $("#small_drag").css("border-color", "black");
+	    $("#small_drag").css("border-style", "solid");
+	    $("#large_drag").css("border-style", "none");
+	    log("Small");
+	    currentY = "small";
+	    return "small";
+	}
+}
